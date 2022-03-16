4f47e3230f6e295eaed657f1f526bd38f9d342db
Mrhung189 <40222634+Mrhung189@users.noreply.github.com>
2022-03-16 10:14:02 +0700

    Add project files.

---
+++ Tuan4_NguyenManhHung1/Controllers/SachController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tuan4_NguyenManhHung1.Models;
+
+namespace Tuan4_NguyenManhHung1.Controllers
+{
+    public class SachController : Controller
+    {
+        // GET: Sach
+        MyDataDataContext data = new MyDataDataContext();
+        public ActionResult ListSach()
+        {
+            var all_sach = from ss in data.Saches select ss;
+            return View(all_sach);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var E_sach = data.Saches.First(m => m.masach == id);
+            return View(E_sach);
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            var E_sach = data.Saches.First(m => m.masach == id);
+            var E_tensach = collection["tensach"];
+            var E_hinh = collection["hinh"];
+            var E_giaban = Convert.ToDecimal(collection["giaban"]);
+            var E_ngaycapnhat = Convert.ToDateTime(collection["ngaycatnhat"]);
+            var E_soluongton = Convert.ToInt32(collection["soluongton"]);
+            E_sach.masach = id;
+            if (string.IsNullOrEmpty(E_tensach))
+            {
+                ViewData["Error"] = "Don't empty!";
+            }
+            else
+            {
+                E_sach.tensach = E_tensach;
+                E_sach.hinh = E_hinh;
+                E_sach.giaban = E_giaban;
+                E_sach.ngaycapnhat = E_ngaycapnhat;
+                E_sach.soluongton = E_soluongton;
+                UpdateModel(E_sach);
+                data.SubmitChanges();
+                return RedirectToAction("ListSach");
+            }
+            return this.Edit(id);
+        }
+        //khi nguoi dung upload hinh anh thi se luu trong thu muc cua project
+        public string ProcessUpload(HttpPostedFileBase file)
+        {
+            if (file == null)
+            {
+                return "";
+            }
+            file.SaveAs(Server.MapPath("~/Content/images/" + file.FileName));
+            return "/Content/images/" + file.FileName;
+        }
+    }
+}
